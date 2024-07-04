6586a91a7ae5a810596e5682af7bef4b8ed8fa06
DESKTOP-8U2BNO6\Hayato Kojima <majiko2627kojima@gmail.com>
2024-07-04 15:00:02 +0000

    hmr config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    port: 3000
+  },
+  vite: {
+    server: {
+      hmr: {
+        port: 5173,
+        clientPort: 443,
+        protocol: 'wss',
+        path: 'hmr/',
+      },
+    },
+  },
+  hooks: {
+    'vite:extendConfig'(viteInlineConfig, env) {
+      viteInlineConfig.server = {
+        ...viteInlineConfig.server,
+        hmr: {
+          port: 5173,
+          clientPort: 443,
+          protocol: 'wss',
+          path: 'hmr/',
+        },
+      }
+    },
+  },
+  compatibilityDate: '2024-07-04',
 })
